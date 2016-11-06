b33b9ebf1f8c9e0f88dc62e71ce619650c73d21b
Barbara Liau <bj.liau@gmail.com>
2016-11-05 22:25:35 -0600

    Import axios. Promisify routes and move internal references to state

--- src/api/user.js
+++ src/api/user.js
@@ -1,111 +1,77 @@
 import { API_URL } from '../../config/constants'
 import { router } from '../main'
-
-let localStorage = window.localStorage
+import axios from 'axios'
 
 export default {
-  // User object will let us check authentication status
-  user: {
-    authenticated: false,
-    userId: ''
-  },
-
-  band: {
-    added: false,
-    activeBandId: ''
-  },
-
-  member: {
-    added: false
-  },
-
   getUser (context) {
-    context.axios.get(API_URL + '/user')
-    .then((user) => {
-      console.log('user is: ', user)
-    })
-    .catch((err) => {
-      console.log('Error checking user ', err)
+    return new Promise((resolve, reject) => {
+      axios
+        .get(API_URL + '/user')
+        .then(result => resolve(result.data.user))
+        .catch((err) => console.log('Error getting user ', err))
     })
   },
 
   loginUser (user, context) {
-    context.axios.post(API_URL + '/user/login', user)
-    .then((user) => {
-      localStorage.setItem('token', user.body.token)
-      this.user.authenticated = true
-      this.user.userId = user.body.user.user_id
-      this.band.activeBandId = user.body.user.active_band_id
-      this.band.added = true
-      this.member.added = true
-      this.member.added = true
-      console.log('user is: ', this.user.userId)
-      console.log('user is: ', this.band.activeBandId)
-      router.push('/dashboard/shows')
-    })
-    .catch((err) => {
-      console.log('Error logging in user: ', err)
+    return new Promise((resolve, reject) => {
+      axios
+        .post(API_URL + '/user/login', user)
+        .then(result => resolve(result.data.user))
+        .catch((err) => console.log('Error logging in user: ', err))
     })
   },
 
   signup (user, context) {
-    context.axios.post(API_URL + '/user/signup', user)
-    .then((user) => {
-      console.log('User logged in')
-      console.log(user.status)
-      localStorage.setItem('token', user.body.token)
-      this.user.authenticated = true
-      console.log(this.band.added)
-      console.log('ifsuccess: ' + this.user.authenticated)
-      router.push('onboarding')
-    })
-    .catch((err) => {
-      console.log(user)
-      console.log('Error signing up user: ', err)
-      // add error handling for error signing up user
-      router.push('signup')
+    return new Promise((resolve, reject) => {
+      axios
+        .post(API_URL + '/user/signup', user)
+        .then(result => resolve(result.data.user))
+        .catch((err) => {
+          console.log('Error signing up user: ', err)
+          // add error handling for error signing up user
+          router.push('signup')
+        })
     })
   },
 
   createNewBand (band, context) {
-    context.axios.post(API_URL + '/api/band/createNewBand', band)
-    .then((user) => {
-      console.log('User logged in')
-      console.log(band)
-      this.band.added = true
-      console.log('ifsuccess: ' + this.user.authenticated)
-    })
-    .catch((err) => {
-      console.log(band)
-      console.log('Band did NOT added: ', err)
+    return new Promise((resolve, reject) => {
+      axios
+        .post(API_URL + '/api/band/createNewBand', band)
+        .then(result => resolve(result))
+        .catch((err) => {
+          console.log(band)
+          console.log('Band did NOT added: ', err)
+        })
     })
   },
 
   checkAuth () {
-    var jwt = localStorage.getItem('token')
-    if (jwt) {
-      this.user.authenticated = true
-      console.log(router)
-      console.log('AUTH true')
-    } else {
-      this.user.authenticated = false
-      console.log('AUTH false')
-    }
+    return new Promise((resolve, reject) => {
+      var jwt = window.localStorage.getItem('token')
+      if (jwt) {
+        this.user.authenticated = true
+        console.log(router)
+        console.log('AUTH true')
+      } else {
+        this.user.authenticated = false
+        console.log('AUTH false')
+      }
+    })
   },
 
-  logoutUser (context) {
-    context.axios.get(API_URL + '/user/logout')
-    .then(() => {
-      console.log('user logged out')
-      localStorage.removeItem('token')
-      this.user.authenticated = false
-      router.push('/')
+  logoutUser () {
+    return new Promise((resolve, reject) => {
+      axios
+        .get(API_URL + '/user/logout')
+        .then(() => resolve(true))
+        .catch(err => reject(err))
     })
   },
 
   getAuthHeader () {
     return {
-      'Authorization': 'Bearer ' + localStorage.getItem('token')
+      'Authorization': 'Bearer ' + window.localStorage.getItem('token')
     }
   }
 
@@ -116,10 +82,10 @@ export default {
 
   // Send a request to the login URL and save the returned JWT
   login (context, creds, redirect) {
-    context.axios.post(API_URL + '/user', creds, (data) => {
+    axios.post(API_URL + '/user', creds, (data) => {
       this.user.authenticated = true
     }).error((err) => {
-      context.error = err
+      error = err
     })
   } */
 
